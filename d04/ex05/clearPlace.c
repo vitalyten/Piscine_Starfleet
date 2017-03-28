0e191bc1a99e2daf6ce257817f7e8cee1bb866b8
VitalyTen <vtenigin@student.42.us.org>
2017-03-27 22:10:34 -0700

    ex05

--- d04/ex05/clearPlace.c
+++ d04/ex05/clearPlace.c
@@ -6,7 +6,7 @@
 /*   By: vtenigin <vtenigin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/03/27 21:58:39 by vtenigin          #+#    #+#             */
-/*   Updated: 2017/03/27 22:01:12 by vtenigin         ###   ########.fr       */
+/*   Updated: 2017/03/27 22:09:39 by vtenigin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ unsigned int clearPlace(unsigned int parkingRow, int pos)
 	int i = 1;
 
 	i <<= pos;
-	parkingRow ^= i;
+	parkingRow &= parkingRow ^ i;
 	return (parkingRow);
 }
